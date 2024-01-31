fb2850853b1a9810051644c8feaf6f0df7502702
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-01-31 16:00:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 
 function swapTheme() {
 	document.getElementById('app').className=document.getElementById('app').className==="day"?"night":"day"
+	document.getElementById('swap').className=document.getElementById('swap').className==="button_day"?"button_night":"button_day"
 }
